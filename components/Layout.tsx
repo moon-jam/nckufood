6f1ae0cf3c762e99cc13b1b5519bf30ec7de2b98
moonjam <wu.messi.wu@gmail.com>
2025-04-12 22:05:13 +0800

    Add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import Footer from './Footer';
 import styles from '../styles/Layout.module.css';
 import { GoogleAnalytics } from '@next/third-parties/google'
@@ -10,6 +11,13 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className={styles.layout}>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <GoogleAnalytics gaId="G-N9BTT1B82K" />
       {/* Navbar / Header */}
       <main className={styles.mainContent}>{children}</main>
