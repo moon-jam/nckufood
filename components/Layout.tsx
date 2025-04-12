df8e22981873f46b808a74e262d0ce23744452ac
moonjam <wu.messi.wu@gmail.com>
2025-04-12 20:19:15 +0800

    Add google Analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './Footer';
 import styles from '../styles/Layout.module.css';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className={styles.layout}>
+      <GoogleAnalytics gaId="G-N9BTT1B82K" />
       {/* Navbar / Header */}
       <main className={styles.mainContent}>{children}</main>
       <Footer />
